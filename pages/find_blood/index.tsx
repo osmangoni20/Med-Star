0d85e0c3d66c3994639f1ab85ee989665ab3f28e
Mohammad Osman Goni <osmangoni0827@gmail.com>
2022-07-29 22:44:57 +0600

    debug  error

--- pages/find_blood/index.tsx
+++ pages/find_blood/index.tsx
@@ -123,8 +123,8 @@ const FindBlood = ({ data }: any) => {
           </div>
           <hr />
           <div className="grid grid-cols-3 gap-4">
-            {data.map((member: any) => (
-              <div>
+            {data.map((member: any, index: number) => (
+              <div key={index}>
                 <div className={`${style.bloodMemberCart} card  shadow`}>
                   <div className={`${style.memberCartBody} card-body `}>
                     <div className={`${style.cartBodyText}`}>
