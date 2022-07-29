886769ecc59296bd65c7041e5dde7acc266e8605
Mohammad Osman Goni <osmangoni0827@gmail.com>
2022-07-29 17:29:33 +0600

    update appointment page

--- pages/doctorAppointment/[id].tsx
+++ pages/doctorAppointment/[id].tsx
@@ -1,20 +1,21 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import Footer from "../../components/common/Footer";
 import Header from "../../components/common/Header/Header";
 import Meta from "../../components/common/Meta";
 import AppointmentHeader from "../../components/Doctor/Appointments/AppointmentHeader/Appointmentheader";
 import BookingAppointment from "../../components/Doctor/Appointments/BookingAppointment/BookingAppointment";
-import WithAuth from "../../components/hooks/RouteProtection";
 
 const Appointment = ({ data }: { data: any }) => {
   const [doctorData, setDoctorData] = useState({});
-  const [SelectedDate, setSelectedDate] = useState<Date>(new Date());
+  const [SelectedDate, setSelectedDate] = useState(new Date());
   const HandleOnChange = (date: any) => {
     setSelectedDate(date);
   };
-  if (!data.camberTime) {
-    setDoctorData({ ...doctorData, patientTime: "3.00pm - 8.00pm" });
-  }
+  useEffect(() => {
+    if (!data.camberTime) {
+      setDoctorData({ ...doctorData, patientTime: "3.00pm - 8.00pm" });
+    }
+  }, []);
   return (
     <div>
       <Meta
@@ -24,7 +25,7 @@ const Appointment = ({ data }: { data: any }) => {
       />
       <Header />
       <AppointmentHeader HandleOnChange={HandleOnChange}></AppointmentHeader>
-      <BookingAppointment />
+      <BookingAppointment date={SelectedDate} data={data}></BookingAppointment>
       <Footer />
     </div>
   );
@@ -40,4 +41,4 @@ export async function getServerSideProps(ctx: { params: { id: any } }) {
   // Pass data to the page via props
   return { props: { data } };
 }
-export default WithAuth(Appointment);
+export default Appointment;
